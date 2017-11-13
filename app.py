1e1c2a9988067891247c14c5b418f120c6d94a3f
Yuyang Zhang <yzhang4@stuy.edu>
2017-11-13 08:41:13 -0500

    Updated jinja

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ import urllib2
 app = Flask(__name__)
 
 @app.route("/")
-def form():
+def form(i):
     uResp = urllib2.urlopen("https://gateway.marvel.com:443/v1/public/characters?apikey=d374bf260e04c8b56a55836e28e8f802")
     string_info = uResp.read()
     dic_info = json.loads(string_info)
-    return render_template('home.html', copy_right = dic_info["copyright"], items = dic_info["items"], info = dic_info["results"])
+    return render_template('home.html', id = i,  name = dic_info["name"], description = dic_info["description"], thumbnail = dic_info["thumbnail"])
 
 if __name__ == "__main__":
     app.debug = True
