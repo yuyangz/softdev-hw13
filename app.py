7cc3144d17471a5f9d46738e58677cdd657e3228
Yuyang Zhang <yzhang4@stuy.edu>
2017-11-13 08:11:24 -0500

    Changed api

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 '''
 Yuyang Zhang
 SoftDev1 pd07
-HW13 -- A RESTful Journey Skyward
-2017-11-09  
+HW14 -- Getting More REST
+2017-11-12  
 '''
 
 from flask import Flask, render_template
@@ -13,10 +13,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def form():
-    uResp = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=s5MDaZjde0L3q7Qcw4aAU0yOx7Git4aHWHcCXaIP")
+    uResp = urllib2.urlopen("https://gateway.marvel.com:443/v1/public/characters?apikey=d374bf260e04c8b56a55836e28e8f802")
     string_info = uResp.read()
     dic_info = json.loads(string_info)
-    return render_template('home.html', title = dic_info["title"], copy_right = dic_info["copyright"], date = dic_info["date"], explanation = dic_info["explanation"], image1= dic_info["hdurl"], image2=dic_info["url"])
+    return render_template('home.html', name = dic_info["name"], description = dic_info["description"], thumbnail = dic_info["thumbnail"])
 
 if __name__ == "__main__":
     app.debug = True
